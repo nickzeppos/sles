87fd5c82b220bc0ef998683bf5ba0ca095c32420
Nick Zeppos <zepposn@gmail.com>
2026-01-02 09:14:59 -0500

    Delete old files

--- test.R
+++
@@ -1,6 +0,0 @@
-# read a .csv file from the dropbox to see if I need to make the file available
-# offline before a script interacts with it to use its contents
-
-path <- "/mnt/c/Users/zeppo/Dropbox/SLES/State Legislative Data/States/WY/WY_Bill_Details_2023.csv"
-df <- read.csv(path)
-head(df)
